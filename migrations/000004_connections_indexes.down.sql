25e9432ce8cfe9ef6ea6accb56224bad8599d655
German Feskov <g.feskov@yandex.ru>
2026-05-12 03:33:32 +0300

    feat(migrations): add indexes on connections log_id and port_b_id

---
+++ migrations/000004_connections_indexes.down.sql
@@ -0,0 +1,2 @@
+DROP INDEX IF EXISTS idx_connections_port_b_id;
+DROP INDEX IF EXISTS idx_connections_log_id;
---
+++ migrations/000004_connections_indexes.up.sql
@@ -0,0 +1,2 @@
+CREATE INDEX idx_connections_log_id    ON connections (log_id);
+CREATE INDEX idx_connections_port_b_id ON connections (port_b_id);
